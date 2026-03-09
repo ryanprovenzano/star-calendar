f0109361fed63913fe47c33cd8f08f955a6904f5
Ryan <ryan.provenz@gmail.com>
2026-03-09 03:42:10 -0400

    Fix mobile dragging and rename desktop script

--- main_mobile.js
+++ main_mobile.js
@@ -1,6 +1,3 @@
-const input = document.getElementById("input");
-const output = document.getElementById("output");
-
 const card = document.getElementById('card');
 
 let startX = 0, startY = 0, deltaX = 0, deltaY = 0;
@@ -9,22 +6,25 @@ let startX = 0, startY = 0, deltaX = 0, deltaY = 0;
 card.addEventListener('touchstart', touchStart);
 
 function touchStart(e) {
-    startX = e.clientX;
-    startY = e.clientY;
+    const touch = e.touches[0];
+    startX = touch.clientX;
+    startY = touch.clientY;
 
     card.addEventListener('touchmove', touchMove);
     card.addEventListener("touchend", touchEnd);
 }
 
 function touchMove(e) {
+    e.preventDefault();
+    const touch = e.touches[0];
 
     // Get deltas
-    deltaX = startX - e.clientX;
-    deltaY = startY - e.clientY;
+    deltaX = startX - touch.clientX;
+    deltaY = startY - touch.clientY;
 
     // Store new mouse position
-    startX = e.clientX;
-    startY = e.clientY;
+    startX = touch.clientX;
+    startY = touch.clientY;
 
     card.style.top = (card.offsetTop - deltaY) + 'px'
     card.style.left = (card.offsetLeft - deltaX) + 'px'
